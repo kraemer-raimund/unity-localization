2e92eef5b120336059aba6dc1b741d48c0b4f8ce
kraemer-raimund <kraemer_raimund@web.de>
2020-07-25 13:41:07 +0200

    Implement localization system with asynchronous loading example

---
+++ Localization/Assets/Localization/AbstractLocalizedText.cs
@@ -0,0 +1,34 @@
+﻿/*
+MIT License
+Copyright (c) 2020 Raimund Krämer
+For the full license text please refer to the LICENSE file.
+ */
+
+using System.Globalization;
+using UnityEngine;
+
+namespace Rakrae.Unity.Localization
+{
+    public abstract class AbstractLocalizedText : MonoBehaviour
+    {
+        [SerializeField] private string _localizedStringKey = null;
+        [SerializeField] private bool _useCustomCulture = false;
+        [SerializeField] private string _culture = null;
+
+        protected string LocalizedString
+        {
+            get
+            {
+                if (_useCustomCulture)
+                {
+                    CultureInfo culture = CultureInfo.GetCultureInfo(_culture);
+                    return Localization.GetString(_localizedStringKey, culture);
+                }
+
+                return Localization.GetString(_localizedStringKey);
+            }
+        }
+
+        protected abstract void SetUIText();
+    }
+}
---
+++ Localization/Assets/Localization/Localization.cs
@@ -0,0 +1,45 @@
+﻿/*
+MIT License
+Copyright (c) 2020 Raimund Krämer
+For the full license text please refer to the LICENSE file.
+ */
+
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Rakrae.Unity.Localization
+{
+    public static class Localization
+    {
+        private static IDictionary<CultureInfo, IDictionary<string, string>> _localizedStringsByLanguage =
+            new Dictionary<CultureInfo, IDictionary<string, string>>();
+
+        private static CultureInfo _customLanguage;
+
+        public static string GetString(string key, CultureInfo culture = null)
+        {
+            CultureInfo language = culture ?? _customLanguage ?? CultureInfo.CurrentCulture;
+
+            if (_localizedStringsByLanguage.TryGetValue(language, out var localizedStrings)
+)
+            {
+                if (localizedStrings.TryGetValue(key, out var value))
+                {
+                    return value;
+                }
+            }
+
+            return "";
+        }
+
+        public static async Task LoadStrings(ILocalizationLoader localizationLoader)
+        {
+            _localizedStringsByLanguage = await localizationLoader.LoadLocalizedStrings();
+        }
+
+        public static void SetCustomLanguage(CultureInfo language) => _customLanguage = language;
+
+        public static void ResetToSystemLanguage() => _customLanguage = CultureInfo.CurrentCulture;
+    }
+}
